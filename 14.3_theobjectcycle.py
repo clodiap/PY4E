3312efff2c02533ebd8b99e8d6ac6fc5b4a59204
clodiap <clodiapp@gmail.com>
2018-08-13 21:11:00 +0200

    Object life cycle

--- 14.3_theobjectcycle.py
+++ 14.3_theobjectcycle.py
@@ -1,15 +1,15 @@
 class PartyAnimal:
-  x = 0
+    x = 0
 
-  def __init__(self):
-    print('I am constructed')
+    def __init__(self):
+        print('I am constructed')
 
-  def party(self):
-    self.x = self.x + 1
-    print('So far', self.x)
+    def party(self):
+        self.x = self.x + 1
+        print('So far', self.x)
 
-  def __del__(self):
-    print('I am destructed', self.x)
+    def __del__(self):
+        print('I am destructed', self.x)
 
 
 an = PartyAnimal()
@@ -17,3 +17,28 @@ an.party()
 an.party()
 an = 42
 print('an contains', an)
+
+print("***" * 50)
+
+class PartyAnimal2:
+    x = 0
+    name = ""
+
+    def __init__(self, z):
+        self.name = z
+        print(self.name, 'constructed')
+
+    def party(self):
+        self.x = self.x + 1
+        print(self.name, 'party count', self.x)
+
+    # def __del__(self):
+    #     print('I am destructed', self.x)
+
+
+s = PartyAnimal2("Sally")
+s.party()
+
+j = PartyAnimal2("Jim")
+j.party()
+j.party()
